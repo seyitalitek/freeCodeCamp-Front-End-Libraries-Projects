b8f9c0b95099124032234092305c05a38b88702f
seyitali <seyitalitek@gmail.com>
2021-03-06 19:12:33 +0100

    formatting with prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,51 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {
-    CleanWebpackPlugin
-} = require('clean-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        index: './src/index.jsx',
-    },
+  mode: "production",
+  entry: {
+    index: "./src/index.jsx",
+  },
 
-    devServer: {
-        contentBase: './docs',
-    },
-    plugins: [
-        new CleanWebpackPlugin({
-            cleanStaleWebpackAssets: false
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Random Quote Machine',
-            template: './src/index.html'
-        })
+  devServer: {
+    contentBase: "./docs",
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
+    new HtmlWebpackPlugin({
+      title: "Random Quote Machine",
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "docs"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        include: path.resolve(__dirname, "src"),
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        include: path.resolve(__dirname, "src"),
+        type: "asset/resource",
+      },
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, "src"),
+        use: "babel-loader",
+      },
     ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'docs'),
-    },
-    module: {
-        rules: [{
-            test: /\.css$/,
-            include: path.resolve(__dirname, 'src'),
-            use: ['style-loader', 'css-loader']
-        }, {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            include: path.resolve(__dirname, 'src'),
-            type: 'asset/resource'
-        }, {
-            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-            include: path.resolve(__dirname, 'src'),
-            type: 'asset/resource'
-        }, {
-            test: /\.(js|jsx)$/,
-            include: path.resolve(__dirname, 'src'),
-            use: 'babel-loader'
-        }]
-    }
+  },
 };
